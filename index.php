eac810d63615cb85c87cd68e2e387a8f72f94c0d
Andrew Rohde <a.rohde89@gmail.com>
2015-08-12 20:55:56 -0500

    merge live website

--- index.php
+++ index.php
@@ -1,109 +1,10 @@
-<!doctype html>
-
-<html lang="en">
-  
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-      <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height, target-densitydpi=medium-dpi" />
-    <meta name="description">
-    <meta name="author">
-    <meta name="keywords" content="madwindevelopers, mw, madwin, developers, android, development, applications, apps">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#109f49">
-    <link rel="shortcut icon" href="img/favicon/favicon.ico">
-    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <link rel="shortcut icon" href="assets/img/favicon.png">
-    <title>Madwin Developers</title>
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <!--<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">-->
-    <!-- Custom styles for this template -->
-    <link href="assets/css/main.css" rel="stylesheet" type="text/css">
-    <!-- Fonts from Google Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet" type="text/css" data-cssintegration="font-family:Lato, serif;" data-familyname="Lato">
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!--<link href="assets/css/sh-default.css" rel="stylesheet" default-stylesheet="true" type="text/css">-->
-    <!-- Google Analytics -->
-    <script type="text/javascript">
-      (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] ||
-        function() {
-          (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-      ga('create', 'UA-60457375-1', 'auto');
-      ga('send', 'pageview');
-    </script>
-    <!-- jquery scroll on click -->
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-      $(document).ready(function() { /* navigation click actions*/
-        $('.scroll-link').on('click', function(event) {
-          event.preventDefault();
-          var sectionID = $(this).attr("data-id");
-          scrollToID('#' + sectionID, 750);
-        }); /* scroll to top action*/
-        $('.scroll-top').on('click', function(event) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: 0
-          }, 'slow');
-        }); /* mobile nav toggle*/
-        $('#nav-toggle').on('click', function(event) {
-          event.preventDefault();
-          $('#main-nav').toggleClass("open");
-        });
-      }); /* scroll function*/
 
 
-      function scrollToID(id, speed) {
-        var offSet = 50;
-        var targetOffset = $(id).offset().top - offSet;
-        var mainNav = $('#main-nav');
-        $('html,body').animate({
-          scrollTop: targetOffset
-        }, speed);
-        if (mainNav.hasClass("open")) {
-          mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-          mainNav.removeClass("open");
-        }
-      }
-      if (typeof console === "undefined") {
-        console = {
-          log: function() {}
-        };
-      }
-    </script>
-    <!-- end jquery scroll on click -->
-  </head>
+<?php
+    
+    include 'website-head.php';
+?>
+
   
   <body style="cursor: auto;">
     <div class="navbar navbar-default navbar-fixed-top">
@@ -155,8 +56,8 @@
           </div>
 		  
           <!-- /col-lg-6 -->
-          <div class="col-sm-6 col-xs-6">
-            <img class="img-responsive" src="assets/img/ipad-hand.png" alt>
+          <div class="col-sm-6 col-xs-offset-1 col-sm-offset-0">
+            <img class="img-responsive" src="assets/img/screenshot_carhud_glare_shadow_525x881.png">
           </div>
           <!-- /col-lg-6 -->
 		  
@@ -170,7 +71,7 @@
     <!-- /headerwrap -->
     <!-- /container -->
     <div id = "apps" class = "page-section container">
-		<h1 class = "col-centered">Apps</h1>
+		<h1>Apps</h1>
 		<div class="row">
 		  
 			<div class="page-section col-sm-4" id="carhud">			  
@@ -213,7 +114,7 @@
 		
       </div>
     </div>
-    <div id="contact" class="page-section container" style="background-color: #DFDFDF">
+    <div id="contact" class="page-section container" style="background-color: #DFDFDF; padding-bottom:12px;">
         <h1 class = "col-centered">Contact</h1>
         <div class="centered">
               <h3>Comment Form</h3>
@@ -222,7 +123,7 @@
 <form role="form" method="post" action="email_comment.php">
 
         <div class = "row">
-            <div class = "col-md-4 col-md-offset-2">
+            <div class = "col-sm-5 col-xs-offset-1 col-sm-offset-1 col-md-offset-2">
               <div class="form-group">
                 <label class="control-label">Name</label>
                 <div class="controls">
@@ -251,7 +152,7 @@
             </div>
 
 
-          <div class = "col-md-4 col-">
+          <div class = "col-sm-5 col-xs-offset-1 col-sm-offset-0">
 
 
               <div class="form-group">
@@ -266,8 +167,11 @@
           </div>
         </div>
         </form>
+</div>
+<div class="container">
     <hr>
 </div>
+
 
 
     <div id="external" class="page-section container">
@@ -317,6 +221,12 @@
     <!-- Bootstrap core JavaScript==================================================- ->        <!-- Placed at the end of the document so the pages load faster -->
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script type="text/javascript" id="hiddenlpsubmitdiv" style="display: none;"></script>
+    <script type="text/javascript">
+        $('.nav a').on('click', function(){
+            $(".btn-navbar").click(); //bootstrap 2.x
+            $(".navbar-toggle").click() //bootstrap 3.x by Richard
+        });
+    </script>
 
   </body>
 
---
+++ website-head.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="en">
+  
+ <head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+      <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height, target-densitydpi=medium-dpi" />
+    <meta name="description">
+    <meta name="author">
+    <meta name="keywords" content="madwindevelopers, mw, madwin, developers, android, development, applications, apps">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#109f49">
+    <link rel="shortcut icon" href="img/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <title>Madwin Developers</title>
+    <!-- Bootstrap core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!--<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">-->
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet" type="text/css">
+    <!-- Fonts from Google Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,900" rel="stylesheet" type="text/css" data-cssintegration="font-family:Lato, serif;" data-familyname="Lato">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!--<link href="assets/css/sh-default.css" rel="stylesheet" default-stylesheet="true" type="text/css">-->
+    <!-- Google Analytics -->
+    <script type="text/javascript">
+      (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] ||
+        function() {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+      ga('create', 'UA-60457375-1', 'auto');
+      ga('send', 'pageview');
+    </script>*/
+<!-- jquery scroll on click -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/scroll_onclick.js"></script>
+<!-- end jquery scroll on click -->
+</head>
+
