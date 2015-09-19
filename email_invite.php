cd5acc09794ed5cc2b0cfd314e7ca633c33d0595
root <a.rohde89@gmail.com>
2015-09-18 19:54:37 -0500

    remove extra code

--- email_invite.php
+++ email_invite.php
@@ -3,17 +3,6 @@
  
 if(isset($_POST['email_invite'])) {
  
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-#    $email_to = "madwin@madwindevelopers.org";
- 
-#    $email_subject = "Mailing List Invite";
- 
-     
- 
-     
  
     function died($error) {
  
@@ -47,8 +36,6 @@ if(isset($_POST['email_invite'])) {
  
     $file = 'email_list';
 
-#    $error_message = "";
- 
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
@@ -74,37 +61,6 @@ if(isset($_POST['email_invite'])) {
     fwrite($file, $add_email);
 
     fclose($file);
- 
-
-#    $email_message = "Form details below.\n\n";
- 
-     
- 
-#    function clean_string($string) {
- 
-#      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-#      return str_replace($bad,"",$string);
- 
-#    }
- 
-
- 
-#    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-
- 
-     
- 
-// create email headers
- 
-#$headers = 'From: '.$email_from."\r\n".
- 
-#'Reply-To: '.$email_from."\r\n" .
- 
-#'X-Mailer: PHP/' . phpversion();
- 
-#@mail($email_to, $email_subject, 'add to email list', $headers);  
  
 ?>
  
