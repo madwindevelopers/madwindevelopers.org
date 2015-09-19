e3fce3633aae30db6d08e656f3a358f2ee663fcb
root <a.rohde89@gmail.com>
2015-09-18 20:36:42 -0500

    don't add email to list if it is already on the list

--- add_to_email_list.php
+++ add_to_email_list.php
@@ -33,7 +33,7 @@ if(isset($_POST['email_invite'])) {
      
  
     $email_from = $_POST['invite_email']; // required
- 
+    $error_message = ""; 
     $file = 'email_list';
 
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -56,26 +56,40 @@ if(isset($_POST['email_invite'])) {
  
     $file = fopen("email_list", "a") or die("Unable to open file!");
 
-    $add_email = $email_from . ";";
+    $add_email = $email_from;
 
-    fwrite($file, $add_email);
+    $emailsraw = file_get_contents("email_list");
+
+    $emailarray = explode(";", $emailsraw);
+
+    $writeNewEmail = TRUE;
+
+    foreach ($emailarray as $value) {
+	if ($add_email == $value) {
+		$writeNewEmail = FALSE;
+		$error_message = "Email already on the list";
+		break;
+	}
+    }
+    if ($writeNewEmail == TRUE) {
+        $add_email = $add_email . ";";
+	fwrite($file, $add_email);
+    }
 
     fclose($file);
+if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+
  
 ?>
- 
- 
- 
-<!-- include your own success html here -->
- 
- 
  
 Thank you for joining the mailing list.
  
  
- 
 <?php
- 
-}
  
 ?>
