86326780fc6cf4237b66f54ba9063022aa798d5b
Andrew Rohde <a.rohde89@gmail.com>
2017-07-08 14:24:45 -0500

    use x-forwarded-for when available

--- api/log-visitor.js
+++ api/log-visitor.js
@@ -2,7 +2,9 @@ var sqlHelper = require('./sql-helper.js');
 
 function logVisitor(req) {
     sqlHelper.connect(function(con) {
-        var sql = "INSERT INTO Visitors (IP) VALUES ('" + req.connection.remoteAddress + "')";
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log("ip : " + ip);
+        var sql = "INSERT INTO Visitors (IP) VALUES ('" + ip + "')";
         con.query(sql, function(err, result) {
             if (err) {
                 console.log(err);
